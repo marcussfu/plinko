acbeb599b6058f9043905990799a542f875d587a
marcussfu <marcussfu@gmail.com>
2024-11-14 17:41:42 +0800

    adjust vite.config.ts and .dockerignore

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'plinko',
+  base: './',//'plinko',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
